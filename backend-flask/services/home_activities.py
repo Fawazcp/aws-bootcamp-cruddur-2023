6d43899bf8df952874bf84e1e87b218fa048a92e
Fawaz C P <fawazmayyil80@gmail.com>
2023-03-06 17:50:12 +0000

    cloudwatch logs

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,11 +4,11 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
+   LOGGER.info("HomeActivities")
    with tracer.start_as_current_span("home-activities-mock-data"):
     span = trace.get_current_span()
     now = datetime.now(timezone.utc).astimezone()
-    span.set_attribute("app.now", now.isoformat()) # this app.now attribute will show inside this span "home-activites-mock-data" , its data is the time now in ISO foramt.
-    
+    span.set_attribute("app.now", now.isoformat()) 
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'fawaz',
